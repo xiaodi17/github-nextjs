c066082527aa4f317a60853c3322d8ff4aacf9b8
David <xiaodi.david@gmail.com>
2020-02-26 20:47:56 +1000

    first commit

---
+++ .next/static/webpack/static/development/pages/index.js.1e473650e99970809ce2.hot-update.js
@@ -0,0 +1,30 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "/Users/Ding/Documents/Imooc/github-nextjs/pages/index.js";
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return __jsx("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 1
+    },
+    __self: this
+  });
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.1e473650e99970809ce2.hot-update.js.map
